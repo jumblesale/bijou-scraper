52547b6e45dee9cc3101e4a326d818014c5dbeba
jumblesale <rob@robsollars.co.uk>
2017-01-31 10:21:20 +0000

    updated job to just print the json

--- job.py
+++ job.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import scraper.generate_data as generate_data
-import pprint
 
 
 categories = generate_data.scrape_data()
 json = generate_data.categories_to_json(categories)
-print(pprint.pformat(json))
+print(json)
